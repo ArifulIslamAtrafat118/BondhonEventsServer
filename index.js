6a9cd766cf82ab07d4a863d888ea8b1ff3d770aa
ArifulIslamAtrafat118 <arafatarifulislam611@gmail.com>
2025-07-12 08:08:34 +0600

    api route setup for event-details

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASS}@cluster0.jcx7gas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
@@ -25,11 +22,24 @@ const run = async () => {
     await client.connect();
 
     const eventsColl = client.db("BondhonEvents").collection("events");
-    app.post("/create-events",(req, res)=>{
-        console.log(req.body);
-        const result = eventsColl.insertOne(req.body)
-        res.send(result)
-    })
+    app.get("/upcoming-events", async (req, res) => {
+      const result = await eventsColl.find().toArray();
+      res.send(result);
+    });
+    app.get("/event-details/:id", async (req, res) => {
+      console.log(req.params.id);
+      const result = await eventsColl.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      // console.log(result);
+      res.send(result);
+    });
+
+    app.post("/create-events", (req, res) => {
+      console.log(req.body);
+      const result = eventsColl.insertOne(req.body);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to mongoDb!");
@@ -40,7 +50,7 @@ const run = async () => {
 run();
 
 app.get("/", (req, res) => {
-  res.send("Wellcome to Plantroy");
+  res.send("Wellcome to Bondhon Events");
 });
 app.listen(PORT, () => {
   console.log(`App is running on PORT: ${PORT}`);
