6bede81cbc85c097c263f4fd6b54a3ee9a81aac5
ArifulIslamAtrafat118 <arafatarifulislam611@gmail.com>
2026-08-04 09:21:26 +0600

    fix: resolve the conflict

--- server.js
+++ server.js
@@ -4,38 +4,17 @@ const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
 const {
-    setCollection
-}=require("./src/controllers/event.controller");
-const {
-    setCollection: setBlogCollection
-}=require("./src/controllers/blog.controller");
-
-
-
-const PORT=process.env.PORT || 4000;
-
-
-
-const startServer=async()=>{
   setCollection: setEventCollection,
 } = require("./src/controllers/event.controller");
+
+const {
+  setCollection: setBlogCollection,
+} = require("./src/controllers/blog.controller");
 
 const {
   setCollection: setPaymentCollection,
 } = require("./src/controllers/payment.controller");
 
-    const db=await connectDB();
-
-
-    setCollection(db);
-    setBlogCollection(db);
-
-
-    app.listen(PORT,()=>{
-
-        console.log(
-            `Server running on PORT ${PORT}`
-        );
 const {
   setCollection: setUserCollection,
 } = require("./src/controllers/user.controller");
@@ -47,6 +26,7 @@ const startServer = async () => {
 
   setUserCollection(db);
   setEventCollection(db);
+  setBlogCollection(db);
   setPaymentCollection(db);
 
   app.listen(PORT, () => {
